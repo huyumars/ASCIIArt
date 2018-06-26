bf5436116303e50b108ec999bbcac0d2bbf3d167
huyumars <huyumars@gmail.com>
2018-06-27 00:16:28 +0800

    haha video
    add template to ImgToChar

--- Project1/CharImgHelper.cpp
+++ Project1/CharImgHelper.cpp
@@ -1,16 +1,17 @@
 #include "CharImgHelper.h"
 #include "ImgUtil.h"
-
-class A {
-public:
-	~A() { std::cout << "I am out" << std::endl; }
-};
-
-A a;
+#include <fstream>
 
 
+std::ofstream out("ouput.txt");
+std::ofstream colorfile("color.txt");
+unsigned char   RGBto256(cv::Vec3b bgr) {
+	int b = bgr[0]/42.7;
+	int g = bgr[1]/42.7;
+	int r = bgr[2]/42.7;
+	return 16 + 36 * r + 6 * g + b;
 
-
+}
 
 CharImgHelper::CharImgHelper(cv::Size imgSize, int sampleRate):
 	sampleSize(cv::Size(imgSize.width*sampleRate / CharWidth, imgSize.height*sampleRate / CharHeight)),
@@ -29,9 +30,24 @@ CharImgHelper::~CharImgHelper()
 cv::Mat & CharImgHelper::processImg(cv::Mat & img)
 {
 	
-	ImgToChar::ResultType & output = imgToCharHelper.getResultString(img,colorMap);
+	ImgToChar<>::ResultType & output = imgToCharHelper.getResultString(img,colorMap);
 	//cv::imshow("color", colorMap);
 	cv::Mat& result = charToimgHelper.charToImg(output,colorMap);
+
+	for (int j = 0; j<output.size(); ++j)
+	{
+		cv::Vec3b *data = colorMap.ptr<cv::Vec3b>(j);//.ptr<uchar>得到的是一个行的指针
+		for (int i = 0; i<output[0].size(); ++i)
+		{
+			unsigned char color = RGBto256( data[i]);
+			colorfile << color;
+		}
+	}
+
+	for (std::string & str : output)
+		out << str << std::endl;
+	out << "----"<<std::endl;
+
 	cv::resize(result, outImg, cv::Size(img.cols, img.rows), 0, 0, cv::INTER_AREA);
 	return outImg;
 }
--- Project1/ImgToChar.h
+++ Project1/ImgToChar.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include  "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -23,22 +22,48 @@ private:
 	std::map<char, double> greyMap;
 	std::map<uchar, char> greyIndex;
 	GrayMap();
-	static GrayMap * _instance ;
+	static std::shared_ptr<GrayMap> instancePtr;
 };
 
 
+template<typename T=std::vector<std::string> >
 class ImgToChar
 {
 public:
-	typedef  std::vector<std::string> ResultType;
-	ResultType & getResultString(cv::Mat& img, cv::Mat &  colorMap);
-	ImgToChar(cv::Size imgSize,cv::Size sampleSize);
-	~ImgToChar();
+	typedef T  ResultType;
+	ResultType & getResultString(cv::Mat& img, cv::Mat &  colorMap) {
+		processImg(img, colorMap);
+		int nl = greyMatCache.rows;
+		int nc = greyMatCache.cols * greyMatCache.channels();
+		//遍历图像的每个像素
+		for (int j = 0; j<nl; j++)
+		{
+			uchar *data = greyMatCache.ptr<uchar>(j);//.ptr<uchar>得到的是一个行的指针
+			for (int i = 0; i<nc; i++)
+			{
+				setCache( j, i, GrayMap::charOfGray(data[i]));
+			}
+		}
+		return outputCache;
+	}
+	ImgToChar(cv::Size imgSize, cv::Size _sampleSize) :
+		orgSize(imgSize),
+		sampleSize(_sampleSize),
+		greyMatCache(cv::Mat(sampleSize, CV_8U)),
+		resizedMatCache(cv::Mat(sampleSize, CV_8U)){
+		initOutputCache(sampleSize.height, sampleSize.width);
+	}
+	~ImgToChar() {};
 private:
 	cv::Size orgSize;
 	cv::Size sampleSize;
 	cv::Mat greyMatCache;
 	cv::Mat resizedMatCache;
 	ResultType outputCache;
+	void processImg(cv::Mat& img, cv::Mat & colorMap);
+	void initOutputCache(size_t height, size_t width) {}
+	inline void setCache(int i, int j, const char & value) {}
 };
+
+
 
